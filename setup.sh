649ccfe0e7ad2b33ae63f317e76b5177dcc8d49b
joseph mahoney <xjoseph.mahoney@gmail.com>
2023-07-26 22:28:23 -0500

    adding modules cmake and build commands

--- setup.sh
+++ setup.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/bash
 git submodule update --init
+
+cmake -S ./modules/glfw -B ./modules/glfw/out/build
+cmake --build ./modules/glfw/out/build
+cmake -S ./modules/vk-bootstrap -B ./modules/vk-bootstrap/out/build
+cmake --build ./modules/vk-bootstrap/out/build
