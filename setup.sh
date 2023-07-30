1ef84bbfac017efaa8fc7d03909d5d85f5557d67
joseph mahoney <xjoseph.mahoney@gmail.com>
2023-07-30 16:18:38 -0500

    Setup modules correctly

--- setup.sh
+++ setup.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/bash
+#
+# SETUP GLFW
 git submodule update --init
+cmake	-S ./modules/glfw \
+	-DCMAKE_INSTALL_PREFIX=./modules \
+	-DCMAKE_INSTALL_INCLUDEDIR=./include \
+	-DCMAKE_INSTALL_LIBDIR=./lib \
+	-B ./modules/glfw/out/build
 
-cmake -S ./modules/glfw -B ./modules/glfw/out/build
 cmake --build ./modules/glfw/out/build
-cmake -S ./modules/vk-bootstrap -B ./modules/vk-bootstrap/out/build
+cmake --install ./modules/glfw/out/build --config "debug"
+#
+# SETUP VK-BOOTSTRAP
+cmake 	-S ./modules/vk-bootstrap \
+	-DCMAKE_INSTALL_PREFIX=./modules \
+	-DCMAKE_INSTALL_INCLUDEDIR=./include \
+	-DCMAKE_INSTALL_LIBDIR=./lib \
+	-B ./modules/vk-bootstrap/out/build \
+
 cmake --build ./modules/vk-bootstrap/out/build
+cmake --install ./modules/vk-bootstrap/out/build --config "debug"
