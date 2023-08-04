db01c92ae88b59ed75fd61a8f772106d49fbabcb
joseph mahoney <xjoseph.mahoney@gmail.com>
2023-08-04 15:52:37 -0500

    adding genaric main and starter CMakeList.txt

---
+++ proj/src/build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+# dir=`git rev-parse --show-toplevel`
+# dir+=`git rev-parse --show-prefix`
+# echo $dir
+
+cmake -S . -B ./out/build
+cmake --build ./out/build
---
+++ proj/src/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+#include <GLFW/glfw3.h>
+#include <VkBootstrap.h>
+#include <VkBootstrapDispatch.h>
+
+int main() {
+
+    std::cout << "hello world" << std::endl;
+
+    return 0;
+}
