9aa09b83c906ede6da5c9a5b9c1076a77b9fd95a
joseph mahoney <xjoseph.mahoney@gmail.com>
2023-08-05 21:00:42 -0500

    linking and basic main with window

--- proj/src/main.cpp
+++ proj/src/main.cpp
@@ -4,9 +4,33 @@
 #include <VkBootstrap.h>
 #include <VkBootstrapDispatch.h>
 
+#define SUCCESS 1
+#define FAIL 0
+
+typedef struct {
+    vkb::InstanceBuilder instance_builder;
+} vk_s;
+
+vk_s vkCtl;
+
+int initWindow (GLFWwindow* window) {
+    int status = SUCCESS;
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+    if ((window = glfwCreateWindow(1024, 1024, "test", NULL, NULL)) == NULL) {
+        std::cout << "window creation FAILED!" << std::endl;
+        status = FAIL;
+    }
+
+    return status;
+}
+
 int main() {
 
-    std::cout << "hello world" << std::endl;
+    GLFWwindow* window = NULL;
+
+    if (initWindow(window)) { std::cout << "success::window created" << std::endl;}
 
     return 0;
 }
