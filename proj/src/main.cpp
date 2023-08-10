30348b221d21c18ef6fce488ef6c87aa1b4c1038
joseph mahoney <xjoseph.mahoney@gmail.com>
2023-08-09 21:09:17 -0500

    intrum - init vulkan

--- proj/src/main.cpp
+++ proj/src/main.cpp
@@ -8,17 +8,20 @@
 #define FAIL 0
 
 typedef struct {
-    vkb::InstanceBuilder instance_builder;
+    GLFWwindow* window;
+    vkb::InstanceBuilder builder;
+    vkb::Instance vkb_inst;
+    vkb::PhysicalDeviceSelector selector;
 } vk_s;
 
-vk_s vkCtl;
+vk_s vkCtl = {};
 
-int initWindow (GLFWwindow* window) {
+int initWindow () {
     int status = SUCCESS;
     glfwInit();
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 
-    if ((window = glfwCreateWindow(1024, 1024, "test", NULL, NULL)) == NULL) {
+    if ((vkCtl.window = glfwCreateWindow(1024, 1024, "test", NULL, NULL)) == NULL) {
         std::cout << "window creation FAILED!" << std::endl;
         status = FAIL;
     }
@@ -26,11 +29,47 @@ int initWindow (GLFWwindow* window) {
     return status;
 }
 
+int initVulkan() {
+    int status = SUCCESS;
+    auto inst_ret = vkCtl.builder.set_app_name ("Example Vulkan Application")
+                        .request_validation_layers ()
+                        .use_default_debug_messenger ()
+                        .build ();
+
+    if (!inst_ret) {
+        status = FAIL;
+        std::cout << "Failed to create Vulkan instance. Error: " << inst_ret.error().message() << "\n";
+        return status;
+    } 
+    else {
+        vkCtl.vkb_inst = inst_ret.value ();
+    }
+
+ 
+
+
+
+
+
+
+
+
+    // if (!phys_ret) {
+    //     status = FAIL;
+    //     std::cerr << "Failed to select Vulkan Physical Device. Error: " << phys_ret.error().message() << "\n";
+    //     return status;
+    // }
+    // else {
+
+    // }
+
+    return status;
+}
+
 int main() {
 
-    GLFWwindow* window = NULL;
-
-    if (initWindow(window)) { std::cout << "success::window created" << std::endl;}
+    if (initWindow()) { std::cout << "success::initWindow" << std::endl;}
+    if (initVulkan()) { std::cout << "success::initVulkan " << std::endl;}
 
     return 0;
 }
