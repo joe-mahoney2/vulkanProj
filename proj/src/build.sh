d1df5628bad8ad76fdaba26f67a4d6f6fcf4efda
joseph Mahoney <xjoseph.mahoney@gmail.com>
2023-09-09 14:24:55 -0500

    adding install call to proj/src

--- proj/src/build.sh
+++ proj/src/build.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/bash
-# dir=`git rev-parse --show-toplevel`
-# dir+=`git rev-parse --show-prefix`
-# echo $dir
-
 cmake -S . -B ./out/build
 cmake --build ./out/build --config Release
+cmake --install ./out/build 
